d38b8e04c61937c641b5ef77edb5d137f16f92c2
itay0421 <itay0421@gmail.com>
2017-11-20 16:12:57 +0200

    Add project files.

---
+++ BL/Class_BlAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BL
+{
+    public class BlAdapter
+    {
+        DAL.IDAL dal = DAL.FactoryDal.getDal();
+
+
+
+        
+
+    }
+}
---
+++ DAL/Class_dal_save_at_list.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Class_dal_save_at_list : IDAL
+    {
+        //idal function
+    }
+}
